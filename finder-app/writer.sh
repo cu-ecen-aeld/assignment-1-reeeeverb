fb75aa6b3ca213f5c43c790f0b9b3849e5cfa6ee
reeeeverb <skydoessoftware@gmail.com>
2022-09-12 18:30:06 -0500

    commit

---
+++ finder-app/writer.sh
@@ -0,0 +1,19 @@
+if [ $# -lt 2 ]
+then
+	echo "Error: No arguments"
+	exit 1
+else
+	WRITEFILE=$1
+	WRITESTR=$2
+fi
+
+#Create file with string
+DIR="$(dirname "${WRITEFILE}")"
+if [ -d "$DIR" ] 
+then
+	echo $(echo $WRITESTR > $WRITEFILE)
+else
+	echo $(mkdir -p $DIR)
+	echo $(echo $WRITESTR > $WRITEFILE)
+fi
+exit 0
